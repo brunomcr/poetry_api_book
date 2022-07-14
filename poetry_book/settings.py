a9fc655c62e483a969dd1d6ae8994d3ee49dc760
bruno <bruno.mcreal@gmail.com>
2022-07-14 19:11:08 -0300

    ALLOWED_HOSTS config (fix)

--- poetry_book/settings.py
+++ poetry_book/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-%sqtdah-1^@y8nalxxhav
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get("DEBUG"))
 
-ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS")]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'poetry-book.herokuapp.com']
 
 
 # Application definition
